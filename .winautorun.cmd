f41aca998c93722dc96d03efd221f8b3355ce4b5
Paul M. Kippes <paul.m.kippes@intel.com>
2020-03-06 14:04:17 -0800

    Add DOSKEY aliases for windoz

---
+++ .winautorun.cmd
@@ -0,0 +1,21 @@
+@echo off
+rem Add this file to c:\
+rem Add a registy autorun entry:
+rem     reg add "HKCU\Software\Microsoft\Command Processor" /v Autorun /d "%HOMEPATH%\.winautorun.cmd" /f
+rem Verify setting with:
+rem     reg query "HKCU\Software\Microsoft\Command Processor" /v Autorun
+
+doskey gls=git log --stat --decorate --graph --abbrev-commit
+doskey glsw=git log --stat --decorate --graph --abbrev-commit --stat-width=120
+doskey gss=git status -s
+doskey glo=git log --graph --format="%%C(auto) %%h %%aE %%d %%s"
+doskey glist=git show --pretty="" --name-only
+
+doskey exp=explorer %HOMEPATH%\projects
+doskey ex.=explorer .
+doskey history=doskey /history
+
+rem ==== work related
+doskey sct=cd %HOMEPATH%\projects\wddm-mx.git\Source\ShaderCompiler\tests\scripts
+
+prompt $P$_$G$S
