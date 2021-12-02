75191a841b3a1d005ffd9470072997fad81536e1
Paul Kippes <kippesp@gmail.com>
2021-12-02 15:14:30 -0800

    allow args for doskey aliases

--- .winautorun.cmd
+++ .winautorun.cmd
@@ -5,11 +5,11 @@ rem     reg add "HKCU\Software\Microsoft\Command Processor" /v Autorun /d "%HOME
 rem Verify setting with:
 rem     reg query "HKCU\Software\Microsoft\Command Processor" /v Autorun
 
-doskey gls=git log --stat --decorate --graph --abbrev-commit
-doskey glsw=git log --stat --decorate --graph --abbrev-commit --stat-width=120
-doskey gss=git status -s
-doskey glo=git log --graph --format="%%C(auto) %%h %%aE %%d %%s"
-doskey glist=git show --pretty="" --name-only
+doskey gls=git log --stat --decorate --graph --abbrev-commit $*
+doskey glsw=git log --stat --decorate --graph --abbrev-commit --stat-width=120 $*
+doskey gss=git status -s $*
+doskey glo=git log --graph --format="%%C(auto) %%h %%aE %%d %%s" $*
+doskey glist=git show --pretty="" --name-only $*
 
 doskey exp=explorer %HOMEPATH%\projects
 doskey ex.=explorer .
