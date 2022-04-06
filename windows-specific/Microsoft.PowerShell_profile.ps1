ccc3f0f8d9c887681cc90ac3e32ec9cd513dca59
Paul Kippes <kippesp@gmail.com>
2022-04-06 08:16:17 -0500

    Windows terminal settings from HSW MBP

---
+++ windows-specific/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+# PowerShell 7.2
+# ==============
+#
+# Show profile search paths:
+#
+# > $profile | Format-List -Force
+#
+#
+#
+# Show PowerShell version
+#
+# > $PSVersionTable.PSVersion
+#
+#
+# > mklink Microsoft.PowerShell_profile.ps1 ..\..\projects\kippesp.github\home_files.git\windows-specific\Microsoft.PowerShell_profile.ps1
+#
+#
+#
+
+Import-Module posh-git
+
+
+
