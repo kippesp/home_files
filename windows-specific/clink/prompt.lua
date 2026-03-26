95e6c8f1ecff643986e68c29de4fdec3e779818f
Paul Kippes <kippesp@gmail.com>
2026-03-26 16:01:03 -0700

    Drain kitty push stack

--- windows-specific/clink/prompt.lua
+++ windows-specific/clink/prompt.lua
@@ -1,4 +1,7 @@
 local p = clink.promptfilter(30)
 function p:filter(prompt)
+    -- Reset kitty keyboard protocol (drains the push stack).
+    -- Fixes garbled input after apps that enable CSI u mode.
+    io.write("\x1b[<10u")
     return os.getcwd().."\n> "
 end
